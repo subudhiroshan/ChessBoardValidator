298deb543a1bf65b97696db6ace657064f28a6bb
GreenDroid <subudhiroshan@gmail.com>
2016-06-24 21:28:16 -0700

    added utility methods for tests

---
+++ src/test/testUtils/ChessTestUtility.java
@@ -0,0 +1,159 @@
+package testUtils;
+
+import main.beans.Coord;
+import main.beans.PieceLocation;
+import main.beans.TeamColor;
+import main.pieces.*;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+
+/**
+ * Utility methods used on the tests for Chess board
+ */
+public class ChessTestUtility {
+
+    public static PieceLocation[][] getInitialChessBoardState(boolean blackon00) {
+        PieceLocation[][] initialChessBoard = initializeChessBoard();
+
+        if (blackon00) {
+            initialChessBoard[0] = placeNonSoldiersInOneRow(0, TeamColor.BLACK, true);
+            initialChessBoard[1] = placeAllSoldiersInOneRow(1, TeamColor.BLACK);
+            initialChessBoard[6] = placeAllSoldiersInOneRow(1, TeamColor.WHITE);
+            initialChessBoard[7] = placeNonSoldiersInOneRow(7, TeamColor.WHITE, true);
+        } else {
+            initialChessBoard[0] = placeNonSoldiersInOneRow(0, TeamColor.WHITE, blackon00);
+            initialChessBoard[1] = placeAllSoldiersInOneRow(1, TeamColor.WHITE);
+            initialChessBoard[6] = placeAllSoldiersInOneRow(1, TeamColor.BLACK);
+            initialChessBoard[7] = placeNonSoldiersInOneRow(7, TeamColor.BLACK, blackon00);
+        }
+        return initialChessBoard;
+    }
+
+    private static PieceLocation[][] initializeChessBoard() {
+        PieceLocation[][] blankChessBoard = new PieceLocation[8][8];
+
+        //initialize board with empty pieces
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                Coord iAmHere = new Coord(i, j);
+
+                PieceLocation blankSpot = new PieceLocation();
+                blankSpot.setPieceType(new EmptyPiece());
+                blankSpot.setLocation(iAmHere);
+
+                blankChessBoard[i][j] = blankSpot;
+            }
+        }
+        return blankChessBoard;
+    }
+
+    private static PieceLocation[] placeAllSoldiersInOneRow(int rowNum, TeamColor teamColor) {
+        PieceLocation[] soldierRow = new PieceLocation[8];
+
+        for(int i=0; i<8; i++) {
+            Coord iAmHere = new Coord(i, rowNum);
+            Soldier aSoldier = new Soldier();
+            aSoldier.setTeamColor(teamColor);
+
+            PieceLocation soldierSpot = new PieceLocation();
+            soldierSpot.setPieceType(aSoldier);
+            soldierSpot.setLocation(iAmHere);
+
+            soldierRow[i] = soldierSpot;
+        }
+        return soldierRow;
+    }
+
+    private static PieceLocation[] placeNonSoldiersInOneRow(int rowNum, TeamColor teamColor, boolean blackOn00) {
+        PieceLocation[] nonSoldierRow = new PieceLocation[8];
+
+        //initializing all the pieces
+        Elephant anElephant = new Elephant();
+        anElephant.setTeamColor(teamColor);
+
+        Horse aHorse = new Horse();
+        aHorse.setTeamColor(teamColor);
+
+        Minister aMinister = new Minister();
+        aMinister.setTeamColor(teamColor);
+
+        Queen aQueen = new Queen();
+        aQueen.setTeamColor(teamColor);
+
+        King aKing = new King();
+        aKing.setTeamColor(teamColor);
+
+        for (int i=0; i<8; i++) {
+            Coord iAmHere = new Coord(i, rowNum);
+
+            PieceLocation nonSoldierSpot = new PieceLocation();
+            nonSoldierSpot.setLocation(iAmHere);
+
+            switch ((int) floor(abs(3.5-i))) {
+                case 3 :
+                    nonSoldierSpot.setPieceType(anElephant);
+                    nonSoldierRow[i] = nonSoldierSpot;
+                    break;
+                case 2 :
+                    nonSoldierSpot.setPieceType(aHorse);
+                    nonSoldierRow[i] = nonSoldierSpot;
+                    break;
+                case 1 :
+                    nonSoldierSpot.setPieceType(aMinister);
+                    nonSoldierRow[i] = nonSoldierSpot;
+                    break;
+                case 0 :
+                    PieceLocation nonSoldierSpotKing = new PieceLocation();
+                    nonSoldierSpotKing.setLocation(iAmHere);
+                    PieceLocation nonSoldierSpotQueen = new PieceLocation();
+                    nonSoldierSpotQueen.setLocation(iAmHere);
+
+                    if (blackOn00) {
+                        if (teamColor.equals(TeamColor.BLACK)) {
+                            nonSoldierSpotQueen.setPieceType(aQueen);
+                            nonSoldierRow[3] = nonSoldierSpotQueen;
+
+                            nonSoldierSpotKing.setPieceType(aKing);
+                            nonSoldierRow[4] = nonSoldierSpotKing;
+                        } else {
+                            nonSoldierSpotQueen.setPieceType(aQueen);
+                            nonSoldierRow[4] = nonSoldierSpotQueen;
+
+                            nonSoldierSpotKing.setPieceType(aKing);
+                            nonSoldierRow[3] = nonSoldierSpotKing;
+                        }
+                    } else {
+                        if (teamColor.equals(TeamColor.WHITE)) {
+                            nonSoldierSpotQueen.setPieceType(aQueen);
+                            nonSoldierRow[3] = nonSoldierSpotQueen;
+
+                            nonSoldierSpotKing.setPieceType(aKing);
+                            nonSoldierRow[4] = nonSoldierSpotKing;
+                        } else {
+                            nonSoldierSpotQueen.setPieceType(aQueen);
+                            nonSoldierRow[4] = nonSoldierSpotQueen;
+
+                            nonSoldierSpotKing.setPieceType(aKing);
+                            nonSoldierRow[3] = nonSoldierSpotKing;
+                        }
+                    }
+                    break;
+            }
+        }
+        return nonSoldierRow;
+    }
+
+    public static void printChessBoard(PieceLocation[][] chessBoardState) {
+
+        for(int i=0; i<8; i++) {
+            System.out.print("|");
+            for(int j=0; j<8; j++) {
+                AcChessPiece currentPiece = chessBoardState[i][j].getPieceType();
+                System.out.print(currentPiece.toString() + "|");
+            }
+            System.out.println();
+        }
+    }
+
+}
