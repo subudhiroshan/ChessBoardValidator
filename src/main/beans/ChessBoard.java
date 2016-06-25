4ce200079968f500db1bb42a9fe02e3fca44fa03
GreenDroid <subudhiroshan@gmail.com>
2016-06-24 20:21:32 -0700

    added validation for chess board state

--- src/main/beans/ChessBoard.java
+++ src/main/beans/ChessBoard.java
@@ -7,14 +7,25 @@ import main.pieces.AcChessPiece;
  */
 public class ChessBoard {
 
-    public static PieceLocation[][] chessBoardState;
+    private static PieceLocation[][] chessBoardState;
 
     public static PieceLocation[][] getChessBoardState() {
         return chessBoardState;
     }
 
-    public static void setChessBoardState(PieceLocation[][] chessBoardState) {
+    public static boolean setChessBoardState(PieceLocation[][] chessBoardState) {
+
+        if (chessBoardState.length != 8) {
+            return false;
+        } else {
+            for(int i=0; i<8; i++) {
+                if (chessBoardState[i].length != 8) {
+                    return false;
+                }
+            }
+        }
         ChessBoard.chessBoardState = chessBoardState;
+        return true;
     }
 
     public static PieceLocation[] getAllPieceLocations(TeamColor team) {
@@ -25,14 +36,7 @@ public class ChessBoard {
             for(int j=0; j<8; j++) {
 
                 PieceLocation currentPieceLocation = chessBoardState[i][j];
-                AcChessPiece currentPieceType = null;
-                try {
-                    currentPieceType = currentPieceLocation.getPieceType().newInstance();
-                } catch (InstantiationException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
+                AcChessPiece currentPieceType = currentPieceLocation.getPieceType();
 
                 if (currentPieceType.getTeamColor().equals(team)) {
                     teamState[k++] = chessBoardState[i][j];
@@ -41,5 +45,4 @@ public class ChessBoard {
         }
         return teamState;
     }
-
 }
